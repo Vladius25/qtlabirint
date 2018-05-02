574e35b0c94676eb7b876507f74d069d1e22e619
Vladius25 <vkorol2509@icloud.com>
2018-05-02 23:41:46 +0500

    New

--- Labirint.pro
+++ Labirint.pro
@@ -1,4 +1,4 @@
-QT -= gui
+QT += gui
 
 CONFIG += c++11 console
 CONFIG -= app_bundle
@@ -14,5 +14,10 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+RESOURCES = res.qrc
+
 SOURCES += \
         main.cpp
+
+DISTFILES += \
+    img.bmp
--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <QCoreApplication>
+#include <QImage>
+#include <QImageReader>
+#include <QDebug>
+#include <QResource>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QImage img("2.bmp");
-    QRgb p = img.pixel(0, 0);
+    //QResource::registerResource("res.qrc");
 
-    cerr << p;
+    QImage img(":/2.bmp");
+
+
+
+    qWarning() << img.load(":/images/img.bmp");
+
+   if (QColor(img.pixel(0,0))==QColor(Qt::black))
+    {
+
+    }
+
+
+   // QRgb p = img.pixel(0, 0);
 
     return a.exec();
 }
